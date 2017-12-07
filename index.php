f01ddf0688776c8c7dacb79385e28bfd1a57c1a8
top1433 <285199@qq.com>
2017-12-07 22:39:59 +0800

    CONFIG COMMIT!

--- index.php
+++ index.php
@@ -3,5 +3,19 @@
 	echo "hello git!";
 	echo "nihao git!";
 
-	define(HOST,localhost);
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PWD', '123456');
+	define('DB_NAME', 'guestbook');
+	define('GB_TABLE_NAME', 'guestbook');
+	define('ADMIN_TABLE_NAME', 'user');
+	define('PER_PAGE_GB', 5);
+	
+
+	$debug=true;
+
+	if($debug){
+		ini_set("display_errors",1);
+		error_reporting(E_ALL);
+	}
  ?>
