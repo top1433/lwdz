5c04455329ac64675931fd56eea00054a196a7c9
top1433 <285199@qq.com>
2017-12-07 22:42:55 +0800

    CONFIG COMMIT!

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php 
+	echo "hello php!";
+	echo "hello git!";
+	echo "nihao git!";
+
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PWD', '123456');
+	define('DB_NAME', 'guestbook');
+	define('GB_TABLE_NAME', 'guestbook');
+	define('ADMIN_TABLE_NAME', 'user');
+	define('PER_PAGE_GB', 5);
+	
+
+	$debug=true;
+
+	if($debug){
+		ini_set("display_errors",1);
+		error_reporting(E_ALL);
+	}
+ ?>
